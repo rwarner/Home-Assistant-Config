e425ef75cb96db93faf70bfffc65c7be56785ad0
Ryan Warner <kentoe@gmail.com>
2026-03-21 13:53:28 -0400

    Improve video alert pipeline, add mobile push, remove dead scripts
    Tighten ffmpeg delays (55s -> 25s), add 8MB size cap for Discord,
    preserve original filenames in conversion. Add mobile push notification
    with camera snapshot alongside Discord video. Remove unused rear/side
    patio capture scripts. Set mode: queued on video alert automation.

--- shell_scripts/recordVideo.sh
+++ shell_scripts/recordVideo.sh
@@ -1,16 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Create the directory we want to use in case it doesn't exist
 mkdir -p /tmp/hass/converted
 
 # For each file in this directory
 for i in /tmp/hass/*.mp4
-do 
-    # Convert it with ffmpeg for under the size limit of Discord's requirements
-    # Usually around ~5mb or so converted with the 1250k birate
-    /usr/bin/ffmpeg -i "$i" -b:v 1250k /tmp/hass/converted/recording.mp4
+do
+    # Get just the filename without path
+    filename=$(basename "$i" .mp4)
+
+    # Convert with ffmpeg, cap at 8MB for Discord free-tier limit
+    /usr/bin/ffmpeg -y -i "$i" -b:v 1250k -fs 8M /tmp/hass/converted/${filename}.mp4 2>/dev/null
 done
 
-# This for some reason is REQUIRED exiting, if you don't want some dumb fucking error from HASS
-# even though the command above works fine
 exit 0
